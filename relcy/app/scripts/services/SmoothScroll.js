add95ba565d954c60f9f669e894a8bb1f2544b56
ashish sahu <asahu@ext-personagraph.com>
2015-05-02 17:00:30 +0530

    scroll to top js fixes changed value 100 to 125

--- relcy/app/scripts/services/SmoothScroll.js
+++ relcy/app/scripts/services/SmoothScroll.js
@@ -18,7 +18,7 @@ angular.module('relcyApp')
         speed = 15;
         var step = Math.round(distance / 25);
         var leapY = stopY > startY ? startY + step : startY - step;
-        stopY = stopY-100;
+        stopY = stopY-125;
         var timer = 0;
         if (stopY > startY) {
             for ( var i=startY; i<stopY; i+=step ) {
