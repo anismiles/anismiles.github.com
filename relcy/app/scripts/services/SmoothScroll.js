457bf7e204194be8f3b15bf2bc01933a185277a3
ashish sahu <asahu@ext-personagraph.com>
2015-05-16 16:18:16 +0530

    scroll fixes

--- relcy/app/scripts/services/SmoothScroll.js
+++ relcy/app/scripts/services/SmoothScroll.js
@@ -19,7 +19,7 @@ angular.module('relcyApp')
             speed = 15;
             var step = Math.round(distance / 25);
             var leapY = stopY > startY ? startY + step : startY - step;
-            stopY = stopY - 106;
+            stopY = stopY - 110;
             var timer = 0;
             if (stopY > startY) {
                 for (var i = startY; i < stopY; i += step) {
