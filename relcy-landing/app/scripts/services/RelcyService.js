87a1eee61fc9b153cf1669e145ecacdf1eeb1bfd
ashish sahu <asahu@ext-personagraph.com>
2015-06-30 19:13:54 +0530

    bg image and ui fixes

--- relcy-landing/app/scripts/services/RelcyService.js
+++ relcy-landing/app/scripts/services/RelcyService.js
@@ -1,20 +1,26 @@
 'use strict';
 
 angular.module('siteApp')
-    .service("RelcyService", ['$timeout', '$q', '$http','$resource', RelcyService]);
+  .service("RelcyService", ['$timeout', '$q', '$http', '$resource', RelcyService]);
 
 /*Session to keep - session specific things*/
-function RelcyService($timeout, $q, $http,$resource) {
-    var APIUrl = "https://staging-w.relcy.com/app";
-	 return $resource(
-            APIUrl,
-            {client_id: "@client_id",platform: "@platform",phone_number:"@phone_number",ambassador_id:"@ambassador_id"},
-            {
-            savePhoneNumber: {
-                method: 'GET',
-                url: APIUrl + '?platform=:platform&phone_number=:phone_number&client_id=:client_id',
-                responseType: 'json'
-            }
-        }
-    );
+function RelcyService($timeout, $q, $http, $resource) {
+  /*staging*/
+  //var APIUrl = "https://staging-w.relcy.com/app";
+  /*dev*/
+  //var APIUrl = "https://dev-w.relcy.com/app";
+  /*api*/
+  var APIUrl = "https://api-w.relcy.com/app";
+
+  return $resource(
+    APIUrl,
+    {client_id: "@client_id", platform: "@platform", phone_number: "@phone_number", ambassador_id: "@ambassador_id"},
+    {
+      savePhoneNumber: {
+        method: 'GET',
+        url: APIUrl + '?platform=:platform&phone_number=:phone_number&client_id=:client_id',
+        responseType: 'json'
+      }
+    }
+  );
 }
