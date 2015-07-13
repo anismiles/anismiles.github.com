c12ac0ea826b9ef4c5a91e44b0425d82b7feb621
ashish sahu <asahu@ext-personagraph.com>
2015-07-13 13:39:07 +0530

    updated file

--- relcy-website/app/scripts/controllers/mainCtrl.js
+++ relcy-website/app/scripts/controllers/mainCtrl.js
@@ -8,7 +8,7 @@
  * Controller of the relcyWebsiteApp
  */
 angular.module('relcyWebsiteApp')
-  .controller('MainCtrl', function ($scope, RelcyService,usSpinnerService,$timeout,$location) {
+  .controller('MainCtrl', function ($scope, RelcyService, usSpinnerService, $timeout, $location) {
     console.log("MainCtrl loaded")
     $scope.phoneNumber = "";
     $scope.platform = "";
@@ -18,11 +18,15 @@ angular.module('relcyWebsiteApp')
     $("#errorAlert").hide();
     $("#seccusAlert").hide();
 
-    $scope.save = function(){
+    $scope.save = function () {
       usSpinnerService.spin('invite');
       //$scope.platform = ( $('#ios').parent().hasClass('active') ? 'ios':'android')
 
-      RelcyService.savePhoneNumber({client_id: "131a22184a",platform:$scope.platform.id,phone_number:$scope.phoneNumber},function(){
+      RelcyService.savePhoneNumber({
+        client_id: "131a22184a",
+        platform: $scope.platform.id,
+        phone_number: $scope.phoneNumber
+      }, function () {
         //
         //$scope.phoneNumber = "";
         //$scope.message = "Link sent to your phone!"
@@ -30,48 +34,44 @@ angular.module('relcyWebsiteApp')
         //mixpanel.track("Landing-Invite-" + $scope.platform);
         mixpanel.track(
           "home_text_link",
-          { "Platform": $scope.platform.id,"Phone-No":$scope.phoneNumber }
+          {"Platform": $scope.platform.id, "Phone-No": $scope.phoneNumber}
         );
-        $scope.message =  "Thank you for your interest. You have been added to our waiting list."
+        $scope.message = "Thank you for your interest. You have been added to our waiting list."
 
         $("#signup form").hide();
         $("#seccusAlert").show();
-        setTimeout(function() {
+        setTimeout(function () {
           $scope.message = ""
-          			$("#hero a.cancel").click();
-          		}, 5000);
+          $("#hero a.cancel").click();
+        }, 5000);
 
         //$timeout(function(){
         //  $scope.phoneNumber = "";
         //  $scope.message = ""
         //},3000)
-      },function(error){
+      }, function (error) {
         //mixpanel.track("Landing-Invite-failed");
 
         $scope.phoneNumber = "";
-        if(error.status == 401)
-        {
+        if (error.status == 401) {
           $scope.message = "Not Authorized"
         }
-        if(error.status == 500)
-        {
+        if (error.status == 500) {
           $scope.message = "Error! Please contact beta@relcy.com"
         }
-        if(error.status == 400)
-        {
+        if (error.status == 400) {
           $scope.message = "Required fields are missing or incorrect number."
         }
-        if(error.status == 403)
-        {
+        if (error.status == 403) {
           $scope.message = "Already sent the SMS twice for this number. Please contact beta@relcy.com"
           mixpanel.track(
             "home_sms_maxout",
-            { "Platform": $scope.platform.id,"Phone-No":$scope.phoneNumber }
+            {"Platform": $scope.platform.id, "Phone-No": $scope.phoneNumber}
           );
         }
         //$("#signup form").hide();
         $("#errorAlert").show();
-        setTimeout(function() {
+        setTimeout(function () {
           $scope.message = ""
           $("#errorAlert").hide();
         }, 5000);
