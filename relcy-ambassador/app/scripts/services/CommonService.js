6f71ee3d82ce4ec229d84d45c04e09e564210263
Animesh Kumar <animesh@47billion.com>
2018-04-25 12:12:00 +0530

    cleanup

--- relcy-ambassador/app/scripts/services/CommonService.js
+++
@@ -1,9 +0,0 @@
-
-'use strict';
-
-angular.module('relcyMobileInvitePageApp')
-    .service("CommonService", ['$timeout', '$q', '$http','$resource', CommonService]); 
- 
-function CommonService() {
-	 this.ambId = ""
-}
