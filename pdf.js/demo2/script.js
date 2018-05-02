e7a81fc46f9b69558064fb7d407c82a8ea883c9d
Devendra Singh <devendra.rathore@47billion.com>
2018-05-02 15:04:43 +0530

    Update script.js

--- pdf.js/demo2/script.js
+++ pdf.js/demo2/script.js
@@ -1,7 +1,7 @@
 // If absolute URL from the remote server is provided, configure the CORS
 // header on that server.
 var url = '//cdn.mozilla.net/pdfjs/tracemonkey.pdf';
-url = 'http://animesh.org/pdf.js/demo/files/merged-200-bat30dj33t2000c1f0cg.pdf?one=two';
+url = 'http://animesh.org/pdf.js/demo/files/merged-200-bat30dj33t2000c1f0cg.pdf?one=three';
 
 // Loaded via <script> tag, create shortcut to access PDF.js exports.
 var pdfjsLib = window['pdfjs-dist/build/pdf'];
